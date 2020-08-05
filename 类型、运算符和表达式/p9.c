f3fc11128a14c98d0fdbdb211be1ae71aa91691c
Leon-zym <leon.zym@qq.com>
2020-08-05 11:56:41 +0800

    .

--- 类型、运算符和表达式/p9.c
+++ 类型、运算符和表达式/p9.c
@@ -1,6 +1,15 @@
+/*
 #include<stdio.h>
 int main()
 {
     printf("整型输出：%d\n",1+2.0);
     printf("浮点型输出：%f\n",1+2.0);
+}
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("integer:%d\n",1+(int)2.0);
+    printf("float:%f\n",1+2.0);
 }
