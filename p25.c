8e520121bf8bb7e4b1eb85987a201bc3394967f3
Leon-zym <leon.zym@qq.com>
2021-02-18 10:45:12 +0800

    continue coding

--- p25.c
+++ p25.c
@@ -6,6 +6,6 @@ int main()
     void *test = p;
     
     printf("*p = %d\n",*p);
-    printf("*test = %d\n",test);
-    printf("hello\n");
+    //printf("*test = %d\n",*test);       //此句错误，void指针不能直接解引用
+    printf("*test = %d\n",*(int *)test);    //(int *)可以先强制转换，然后再解引用
 }
